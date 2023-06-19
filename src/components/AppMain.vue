8ba71510c9222a1ced786a81c26096d82c56146c
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-20 01:07:27 +0200

    imported axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,52 @@
 <template>
 
     <main>
-        <div>
-           <SingleCard/>
+        <div class="container">
+           <div class="container-card">
+            <SingleCard/>
+           </div>
         </div>
     </main>
   
 </template>
 <script>
  import SingleCard from './SingleCard.vue'
+ import axios from 'axios'
+
+ export default {
+    name: 'AppMain',
+    data() {
+        return {
+ 
+           } 
+     }, 
+
+     components: {
+        SingleCard
+     },
+
+     created () {
+        const axios = require('axios');
+
+      // Make a request for a user with a given ID
+       axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+       .then(function (response) {
+       // handle success
+       console.log(response);
+       })
+      .catch(function (error) {
+        // handle error
+       console.log(error);
+       })
+       .finally(function () {
+         // always executed
+         });
+
+     }
+
+
+        
+ }
 </script>
 <style lang="scss">
 @import 'bootstrap/scss/bootstrap'
