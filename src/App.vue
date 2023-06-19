198c7781d96633fa850b1314141824076c55561d
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-20 00:02:25 +0200

    imported bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import CardsList from './components/CardsList.vue'
+import SingleCard from './components/SingleCard.vue'
 </script>
 
 <template>
-  
+  <AppHeader/>
+  <AppMain/>
+  <CardsList/>
+  <SingleCard/>
 </template>
 
-<style>
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap';
+@import './styles/general.scss';
 
 </style>
